aa88879f082f9a521963542ce33ae01c1eb464c3
Lyuboslav Krastev <luboslav.krustev@abv.bg>
2020-03-15 23:30:37 +0200

    Implementing image display

--- src/components/ImageList.js
+++ src/components/ImageList.js
@@ -1,5 +1,13 @@
-import React from 'react'
+import '../css/ImageList.css';
+import React from "react";
+import ImageCard from './ImageCard'
 
-const ImageList = () => {
-    return <div>Images</div>
-}
+const ImageList = props => {
+  const images = props.images.map(image => {
+    return  <ImageCard key={image.id} image={image}/>
+  });
+
+  return <div className="image-list">{images}</div>;
+};
+
+export default ImageList;
