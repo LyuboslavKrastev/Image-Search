17d4ecc05dc505b5ea3a56ab98b2ac83359a6086
Lyuboslav Krastev <luboslav.krustev@abv.bg>
2020-03-23 18:10:01 +0200

    Dynamic image spans

--- src/api/unsplash.js
+++ src/api/unsplash.js
@@ -1,7 +1,7 @@
 import axios from "axios";
 // WARNING: both your Access Key and Secret Key must remain confidential.
 // The canonical way to use such API keys is to get the client side app to send a request to your backend API.
-import { GetAccessKey } from "../secrets/UnsplashSecrets";
+import GetAccessKey from "../secrets/UnsplashSecrets";
 
 export default axios.create({
   baseURL: "https://api.unsplash.com/",
--- src/components/ImageCard.js
+++ src/components/ImageCard.js
@@ -5,16 +5,29 @@ class ImageCard extends React.Component {
     super(props);
 
     this.imageRef = React.createRef();
+    this.state = {
+      spans: 0
+    }
   }
 
   componentDidMount() {
-      console.log(this.imageRef.current.clientHeight);
+    this.imageRef.current.addEventListener('load', this.setSpans);
+  }
+
+  setSpans = () => {
+    const height = this.imageRef.current.clientHeight;
+    let autoRowHeight = 10;
+    const spans = Math.ceil(height / autoRowHeight);
+
+    this.setState({
+      spans
+    })
   }
 
   render() {
     const { description, urls } = this.props.image;
     return (
-      <div>
+      <div style={{gridRowEnd: `span ${this.state.spans}`}}>
         <img ref={this.imageRef} alt={description} src={urls.regular} />
       </div>
     );
